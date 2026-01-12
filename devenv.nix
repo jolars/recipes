55be8cfccb5debd88885b1f52fe69ee5ed164239
Johan Larsson <johan@jolars.co>
2026-01-12 21:53:26 +0100

    chore: update development setup

---
+++ devenv.nix
@@ -0,0 +1,70 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.git
+    pkgs.bashInteractive
+    pkgs.pkg-config
+    pkgs.vips
+    pkgs.libwebp
+    pkgs.rubyPackages.ruby-vips
+    pkgs.pngquant
+    pkgs.imagemagick
+    (pkgs.python3.withPackages (ps: [
+      ps.requests
+      ps.openai
+    ]))
+  ];
+
+  # https://devenv.sh/languages/
+  languages = {
+    ruby = {
+      enable = true;
+      package = pkgs.ruby;
+    };
+  };
+
+  # https://devenv.sh/processes/
+  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+
+  # https://devenv.sh/basics/
+  enterShell = ''
+    hello         # Run scripts directly
+    git --version # Use packages
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
